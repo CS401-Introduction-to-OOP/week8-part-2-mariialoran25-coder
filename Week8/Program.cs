c1e34c9b800fcd9861adef61add450007f2c00fc
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-28 17:23:42 +0200

    Finished Stage 3. And Finished Gc Starter1

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,24 @@
-﻿
+﻿using Week8;
 class Program
 {
     public static void Main()
     {
+        var file = new FileResource("reposts.txt");
+        var network = new NetworkResource("api.compant.local");
+
+        var manager = new ResourceManager<Resource>();
         
+        manager.Add(file);
+        manager.Add(network);
+        
+        manager.OpenAll();
+        using (file)
+        {
+            Console.WriteLine($"Using file {file.IsOpen}");
+        }
+        
+        manager.CloseAll();
+        Console.WriteLine("Done");
     }
     
 }
