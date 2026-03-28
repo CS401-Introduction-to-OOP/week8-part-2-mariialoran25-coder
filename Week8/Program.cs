157f8db468daf50a5b80432c0da14227b078fb47
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-28 17:54:13 +0200

    Finished Stage3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,35 @@
-﻿
+﻿using Week8;
 class Program
 {
     public static void Main()
     {
+        Point p1 = new Point { X = 1, Y = 2 };
+        Point p2 = p1;
+
+        p2.X = 12;
+        p2.Y = 3;
+        Console.WriteLine($"P1: {p1.X}, {p1.Y}");
+        Console.WriteLine($"p2: {p2.X}, {p2.Y}");
         
+        
+
+        Console.WriteLine();
+
+        PointRef r1 = new PointRef { X = 1, Y = 2 };
+        PointRef r2 = r1;
+
+        r2.X = 12;
+        //r2.Y = 25;
+        
+        Console.WriteLine($"R1: {r1.X}, {r1.Y}");
+        Console.WriteLine($"R2: {r2.X}, {r2.Y}");
+
+
+        Console.WriteLine();
+
+        BoxingTester.Test();
+
+
     }
     
 }
