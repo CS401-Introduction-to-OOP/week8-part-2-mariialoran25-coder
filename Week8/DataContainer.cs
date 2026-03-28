f0e123276d8c707356558e82dc26c2de7cb0000a
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-28 18:11:16 +0200

    Finished Stage 2

---
+++ Week8/DataContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace Week8;
+
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> _items = new();
+    
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+    
+    public long GetTotalSize()
+    {
+        long totalsize = 0;
+        foreach (var item in _items)
+        {
+            totalsize += item.GetSize();
+        }
+
+        return totalsize;
+    }
+}
