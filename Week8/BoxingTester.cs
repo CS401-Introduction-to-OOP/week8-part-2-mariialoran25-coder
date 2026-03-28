12c170b6775b9db37973e7c33c4c6e0238295a07
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-28 17:45:09 +0200

    Finished Stage2

---
+++ Week8/BoxingTester.cs
@@ -0,0 +1,17 @@
+﻿namespace Week8;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10;        // Ось тут boxing створення копія в хіпі
+        object obj = a;   // Тобто приведення ValueTypes до Reference Type. Тобто преретворюю а на об'єкт 
+
+                            // Ось тут unboxing, створила нову копію в стек, а оригінал в хіпі
+        int ca = (int)obj;   // тут навпаки приведення Reference Type до ValueTypes
+
+        Console.WriteLine($"a: {a}, obj: {obj}, c: {ca}");
+        
+        
+    }
+}
