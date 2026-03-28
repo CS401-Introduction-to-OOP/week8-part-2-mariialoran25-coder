e55b841760ba785a835cb62db5174a3a8115b925
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-28 17:00:22 +0200

    Add FileResource.cs, but not finished yet

---
+++ Week8/FileResource.cs
@@ -0,0 +1,33 @@
+﻿namespace Week8;
+
+public class FileResource: Resource, IDisposable
+{
+
+    public FileResource(string name) : base(name)
+    {
+        
+    }
+    
+    public override void Open()
+    {
+        IsOpen = true;
+        throw new NotImplementedException();
+    }
+    
+    
+    public override void Close()
+    {
+        IsOpen = false;
+        throw new NotImplementedException();
+    }
+    
+    
+    public void Dispose()
+    {
+        // TODO: call Close() and print diagnostic message
+        throw new NotImplementedException();
+    }
+    
+
+    
+}
