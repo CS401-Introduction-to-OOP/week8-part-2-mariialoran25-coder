e09add2ed188cb07cb71d7ef02dd7cb8820d5fce
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-28 18:01:29 +0200

    Finished Stage1

---
+++ Week8/DataItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Week8;
+
+public class DataItem
+{
+    public string id { get; set; } = string.Empty;
+    public byte[] Payload { get; set; } = Array.Empty<byte>();
+
+    
+    public long GetSize()
+    {
+        return Payload.Length;
+    }
+}
