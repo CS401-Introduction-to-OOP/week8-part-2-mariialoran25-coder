a09c2eb1fb58a8ad5f5b9a0ecb0a0426890e3f48
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-03-28 17:16:01 +0200

    Add ResourceManager.cs

---
+++ Week8/ResourceManager.cs
@@ -0,0 +1,27 @@
+﻿namespace Week8;
+
+public class ResourceManager<T> where T: Resource
+{
+    private readonly List<T> _resources = new();
+    
+    public void Add(T resource)
+    {
+        _resources.Add(resource);
+    }
+
+    public void OpenAll()
+    {
+        foreach (var file in _resources)
+        {
+            file.Open();
+        }
+    }
+
+    public void CloseAll()
+    {
+        foreach (var file in _resources)
+        {
+            file.Close();
+        } 
+    }
+}
